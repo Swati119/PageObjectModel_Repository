16f9af9cc227431c77888057ab3812379d72504c
Pushpak Bhattacharjee <bhattacharjee.pushpak@gmail.com>
2017-03-19 17:00:25 +0530

    Added the maven plugin for running the tests using maven, into the pon file

--- src/com/phptravels/testCases/Login.java
+++ src/com/phptravels/testCases/Login.java
@@ -21,25 +21,11 @@ public class Login {
 	String user = utils.readProperties(propertyFileName, "userName");
 	String password = utils.readProperties(propertyFileName, "password");
 
-
-	
-
-	/*public Login (WebDriver driverObj){
-		CommonUtils commonUtils = new CommonUtils(); 
-		this.user = commonUtils.readProperties(propertyFileName, "userName");
-		this.password = commonUtils.readProperties(propertyFileName, "password");
-		this.driver = driverObj;
-		this.loginPage = new LoginPage(driver);
-	}*/
-
 	@BeforeTest
 	public void LaunchBrowser() throws InterruptedException{
 		
 		WebDriver ffdriver = firefoxBrowser.initializeBrowser();
 		this.driver = ffdriver;
-		
-
-
 	}
 	
 	@Test
